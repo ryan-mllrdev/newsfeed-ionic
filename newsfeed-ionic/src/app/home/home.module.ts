5e1e4e9d7e502a7d65d3127dbebc1e29821b635f
ryan-mllrdev <ryan.repe@mllrdev.com>
2020-07-01 14:30:07 +0800

    Created paths for components and housed newsfeed components into one module.

--- newsfeed-ionic/src/app/home/home.module.ts
+++ newsfeed-ionic/src/app/home/home.module.ts
@@ -1,40 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
-import { NewsfeedComponent } from '../newsfeed/newsfeed.component';
-import { PostFormComponent } from '../newsfeed/post/post-form/post-form.component';
-import { FormModalComponent } from '../newsfeed/post/post-form/form-modal/form-modal.component';
-
-import { OrderModule } from 'ngx-order-pipe';
-import { PostComponent } from '../newsfeed/post/post.component';
-import { PostCommentComponent } from '../newsfeed/post/post-comment/post-comment.component';
-import { PostReactionComponent } from '../newsfeed/post/post-reaction/post-reaction.component';
-import { PostToolbarsComponent } from '../newsfeed/post/post-toolbars/post-toolbars.component';
-import { CommentListToolbarsComponent } from '../newsfeed/post/comment-list/comment-list-toolbars/comment-list-toolbars.component';
-import { CommentListComponent } from '../newsfeed/post/comment-list/comment-list.component';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { NewsfeedNotificationService } from '../services/newsfeed-notification.service';
-import { DistanceToNowPipe } from '../pipes/distance-to-now.pipe';
+import { NewsfeedModule } from '@rdr/newsfeed-module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule, HomePageRoutingModule, OrderModule, Ng2SearchPipeModule],
-  declarations: [
-    HomePage,
-    NewsfeedComponent,
-    PostComponent,
-    PostCommentComponent,
-    PostReactionComponent,
-    CommentListComponent,
-    CommentListToolbarsComponent,
-    PostToolbarsComponent,
-    PostFormComponent,
-    FormModalComponent,
-    DistanceToNowPipe,
-  ],
-  providers: [NewsfeedNotificationService],
+  imports: [CommonModule, NewsfeedModule, HomePageRoutingModule],
+  declarations: [HomePage],
 })
 export class HomePageModule {}
---
+++ newsfeed-ionic/src/app/newsfeed/newsfeed.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OrderModule } from 'ngx-order-pipe';
+import { IonicModule } from '@ionic/angular';
+
+import { NewsfeedComponent } from '@rdr/newsfeed';
+import { PostComponent } from '@rdr/post';
+import { PostCommentComponent } from '@rdr/post-comment';
+import { PostReactionComponent } from '@rdr/post-reaction';
+import { PostToolbarsComponent } from '@rdr/post-toolbar';
+import { PostFormComponent } from '@rdr/post-form';
+import { FormModalComponent } from '@rdr/form-modal';
+import { CommentListComponent } from '@rdr/comment-list';
+import { CommentListToolbarsComponent } from '@rdr/comment-list-toolbar';
+import { DistanceToNowPipe } from '@rdr/pipes/distance-to-now.pipe';
+import { NewsfeedNotificationService } from '@rdr/services/newsfeed-notification.service';
+
+@NgModule({
+  imports: [CommonModule, IonicModule, Ng2SearchPipeModule, FormsModule, ReactiveFormsModule, OrderModule],
+  declarations: [
+    NewsfeedComponent,
+    PostComponent,
+    PostCommentComponent,
+    PostReactionComponent,
+    CommentListComponent,
+    CommentListToolbarsComponent,
+    PostToolbarsComponent,
+    PostFormComponent,
+    FormModalComponent,
+    DistanceToNowPipe,
+  ],
+  exports: [NewsfeedComponent],
+  providers: [NewsfeedNotificationService],
+})
+export class NewsfeedModule {}
